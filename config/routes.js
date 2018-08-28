6b35a4b58c8197fdec7493391a161853392b120f
Donnie Zimmerman <donniez0930@gmail.com>
2018-08-28 11:20:34 -0700

    works

--- config/routes.js
+++ config/routes.js
@@ -3,5 +3,8 @@ const cards = require("../controllers/cards.js")
 module.exports = function(app){
 
   app.get('/', cards.index);
+  app.post('/cards', cards.create);
+  app.get('/cards/add/:id', cards.add);
+  app.get('/cards/remove/:id', cards.remove);
 
 }
--- controllers/cards.js
+++ controllers/cards.js
@@ -3,6 +3,45 @@ const knex = require("../db/knex.js");
 module.exports = {
   // CHANGE ME TO AN ACTUAL FUNCTION
   index: function(req, res) {
-    res.render("index");
+    if(!req.session.deck){
+      req.session.deck = [];
+     }
+     knex('cards')
+      .then((results)=>{
+        res.render('index',{cards: results, deck: req.session.deck})
+      })
+  },
+  create: (req,res)=>{
+    knex('cards')
+     .insert(req.body)
+     .then(()=>{
+       res.redirect('/');
+     })
+  },
+  add: (req,res)=>{
+    knex('cards')
+      .where('id', req.params.id)
+      .then((results)=>{
+        req.session.deck.push(results[0]);
+        res.redirect('/');
+      })
+  },
+  remove: (req,res)=>{
+    let deck = req.session.deck;
+    if(deck.length == 1){
+      req.session.deck = []
+      res.redirect('/')
+      return;
+    }
+    for(i=0; i < deck.length; i++){
+      if(deck[i].id == req.params.id){
+        deck.splice(i,1)
+        req.session.save(()=>{
+          res.redirect('/');
+        })
+        return;
+      }
+    }
+    res.redirect('/');
   },
 }
